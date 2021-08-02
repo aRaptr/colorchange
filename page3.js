59808ee90264873ea3e156619d42dd02a32b1f75
aRaptr <88123689+aRaptr@users.noreply.github.com>
2021-08-02 19:07:59 -0400

    Add files via upload

---
+++ page3.js
@@ -0,0 +1,151 @@
+
+window.onload = toggle;
+function toggle(){
+      
+
+       
+       var black = document.getElementById("black");
+       var white = document.getElementById("white");
+       var blue = document.getElementById("blue");
+       var red = document.getElementById("red");
+       var green = document.getElementById("green");
+       var olive = document.getElementById("olive");
+       var lime = document.getElementById("lime");
+       var maroon = document.getElementById("maroon");
+       var fuchsia = document.getElementById("fuchsia");
+       var yellow = document.getElementById("yellow");
+       var silver = document.getElementById("silver");
+       var gray = document.getElementById("gray");
+       var teal = document.getElementById("teal");
+       var navy = document.getElementById("navy");
+       var aqua = document.getElementById("aqua");
+       var purple = document.getElementById("purple");
+       var serif = document.getElementById("serif");
+       var sansSerif = document.getElementById("sansSerif");
+
+
+     black.onclick = blackf;
+     white.onclick =  whitef;
+     blue.onclick =  bluef;
+     red.onclick =  redf;
+     green.onclick = greenf;
+     olive.onclick = olivef;
+     lime.onclick = limef;
+     maroon.onclick = maroonf;
+     fuchsia.onclick = fuchsiaf;
+     yellow.onclick = yellowf;
+     silver.onclick = silverf;
+     gray.onclick = grayf;
+     teal.onclick = tealf;
+     navy.onclick = navyf;
+     aqua.onclick = aquaf;
+     purple.onclick = purplef;
+     serif.onclick = seriff;
+     sansSerif.onclick=sansSeriff;
+
+    
+    
+    
+     
+}
+
+
+
+function blackf(){
+    
+    document.body.style.backgroundColor = "black" ;
+  
+ }
+ 
+ function whitef(){
+     
+     document.body.style.backgroundColor = "white" ;
+ } 
+ 
+ function bluef(){
+     
+     document.body.style.backgroundColor = "blue" ;
+ } 
+ 
+ function redf(){
+     
+     document.body.style.backgroundColor = "red" ;
+ } 
+ 
+ function greenf(){
+     
+     document.body.style.backgroundColor = "green" ;
+ } 
+ 
+ function olivef(){
+     
+     document.body.style.backgroundColor = "olive" ;
+ } 
+ 
+ function limef(){
+     
+     document.body.style.backgroundColor = "lime" ;
+ } 
+ 
+ function maroonf(){
+     
+     document.body.style.backgroundColor = "maroon" ;
+ } 
+
+function fuchsiaf(){
+    
+    document.body.style.backgroundColor = "fuchsia" ;
+} 
+
+function yellowf(){
+    
+    document.body.style.backgroundColor = "yellow" ;
+} 
+
+function silverf(){
+    
+    document.body.style.backgroundColor = "silver" ;
+} 
+
+function grayf(){
+    
+    document.body.style.backgroundColor = "gray" ;
+} 
+
+function tealf(){
+    
+    document.body.style.backgroundColor = "teal" ;
+} 
+
+function navyf(){
+    
+    document.body.style.backgroundColor = "navy" ;
+} 
+
+function aquaf(){
+    
+    document.body.style.backgroundColor = "aqua" ;
+} 
+
+function purplef(){
+    
+    document.body.style.backgroundColor = "purple" ;
+} 
+function seriff(){
+    
+    document.body.style.fontFamily = "Serif"
+} 
+function sansSeriff(){
+  
+    
+    document.body.style.fontFamily = "Sans-serif" ;
+} 
+
+
+
+
+
+
+
+
+
